1524b495dfef5c49c64dd66b81d1831b69c59b4b
Oscar-Sch <oscarschwerdt@gmail.com>
2023-08-09 12:26:23 -0300

    added repositories and DTOs

--- Backend/src/main/java/com/ensolvers/notesolver/Repositories/TagRepository.java
+++ Backend/src/main/java/com/ensolvers/notesolver/Repositories/TagRepository.java
@@ -1,4 +1,10 @@
 package com.ensolvers.notesolver.Repositories;
 
-public interface TagRepository {
+import com.ensolvers.notesolver.Models.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Integer> {
+    Tag findByName(String name);
 }
