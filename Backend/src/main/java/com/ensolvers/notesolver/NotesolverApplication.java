6fa68fa6f1b30ac9492f3ede19a638e216f608f0
Oscar-Sch <oscarschwerdt@gmail.com>
2023-08-09 09:46:14 -0300

    repository initialized

---
+++ Backend/src/main/java/com/ensolvers/notesolver/NotesolverApplication.java
@@ -0,0 +1,13 @@
+package com.ensolvers.notesolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesolverApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/ensolvers/notesolver/NotesolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensolvers.notesolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
