0f4a4f1bcdb9371b53e591965896312c91bbf2f3
Oscar-Sch <oscarschwerdt@gmail.com>
2023-08-11 15:19:07 -0300

    configured docker with postgre

--- Backend/src/main/java/com/ensolvers/notesolver/NotesolverApplication.java
+++ Backend/src/main/java/com/ensolvers/notesolver/NotesolverApplication.java
@@ -15,19 +15,19 @@ public class NotesolverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NotesolverApplication.class, args);
 	}
-	@Bean
-	public CommandLineRunner initData(NoteService noteService, TagService tagService) {
-		return (args) -> {
-			Note note1 = new Note("Todo List", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex eaque amet praesentium asperiores dolores dicta molestias, facere laborum assumenda! Expedita inventore neque asperiores officia ullam recusandae nemo beatae rem cum.");
-			Note note2 = new Note("Grocery buys", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex eaque amet praesentium asperiores dolores dicta molestias, facere laborum assumenda! Expedita inventore neque asperiores officia ullam recusandae nemo beatae rem cum.");
-			Note note3 = new Note("New story ideas", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex eaque amet praesentium asperiores dolores dicta molestias, facere laborum assumenda! Expedita inventore neque asperiores officia ullam recusandae nemo beatae rem cum.");
-			Note note4 = new Note("Thoughts I had", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex eaque amet praesentium asperiores dolores dicta molestias, facere laborum assumenda! Expedita inventore neque asperiores officia ullam recusandae nemo beatae rem cum.");
-
-			noteService.createNote(new NoteDTO(note1));
-			noteService.createNote(new NoteDTO(note2));
-			noteService.createNote(new NoteDTO(note3));
-			noteService.createNote(new NoteDTO(note4));
-
-		};
-	}
+//	@Bean
+//	public CommandLineRunner initData(NoteService noteService, TagService tagService) {
+//		return (args) -> {
+//			Note note1 = new Note("Todo List", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex eaque amet praesentium asperiores dolores dicta molestias, facere laborum assumenda! Expedita inventore neque asperiores officia ullam recusandae nemo beatae rem cum.");
+//			Note note2 = new Note("Grocery buys", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex eaque amet praesentium asperiores dolores dicta molestias, facere laborum assumenda! Expedita inventore neque asperiores officia ullam recusandae nemo beatae rem cum.");
+//			Note note3 = new Note("New story ideas", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex eaque amet praesentium asperiores dolores dicta molestias, facere laborum assumenda! Expedita inventore neque asperiores officia ullam recusandae nemo beatae rem cum.");
+//			Note note4 = new Note("Thoughts I had", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex eaque amet praesentium asperiores dolores dicta molestias, facere laborum assumenda! Expedita inventore neque asperiores officia ullam recusandae nemo beatae rem cum.");
+//
+//			noteService.createNote(new NoteDTO(note1));
+//			noteService.createNote(new NoteDTO(note2));
+//			noteService.createNote(new NoteDTO(note3));
+//			noteService.createNote(new NoteDTO(note4));
+//
+//		};
+//	}
 }
